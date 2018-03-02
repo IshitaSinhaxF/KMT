09920c2d431f3485e36a8f514614be7a5e9bbf09
Likhita <patralikhita@gmail.com>
2018-03-02 15:14:22 -0800

    Added child routing in app-routing.modules

--- WebApplication/src/app/app-routing.module.ts
+++ WebApplication/src/app/app-routing.module.ts
@@ -7,18 +7,27 @@ import { HomeComponent } from './home/home.component';
 import { PendingForApprovalComponent } from './article/pending-for-approval/pending-for-approval.component';
 import { ViewAricleComponent } from './article/view-aricle/view-aricle.component';
 import { CreateArticleComponent } from './article/create-article/create-article.component';
+import { PublishedComponent } from './article/published/published.component';
+import { DraftArticleComponent } from './article/draft-article/draft-article.component';
 const ROUTES: Routes = [
   { path: '', component: LoginComponent },
   {
-    path: 'home', component: HomeComponent,
+    path: 'home',
+    component: HomeComponent,
+    children: [
+                { path: 'publishedArticles', component: PublishedComponent },
+                { path: 'Pending', component: PendingForApprovalComponent },
+                { path: 'Draft', component: DraftArticleComponent }
+            ]
   },
 
   { path: 'viewArtcile', component: ViewAricleComponent },
-  { path: 'createArticle' , component: CreateArticleComponent },
+  { path: 'createArticle', component: CreateArticleComponent },
 
 ]
 @NgModule({
   exports: [RouterModule],
-  imports: [RouterModule.forRoot(ROUTES)]
+  imports: [
+    RouterModule.forRoot(ROUTES)]
 })
 export class AppRoutingModule { }
