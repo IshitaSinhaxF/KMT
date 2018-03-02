0f4cad3c90befe2c349100e33d54eaf86ce0479c
Ananya2017 <anandi@xfusiontech.com>
2018-03-02 10:02:33 -0800

    create new article page

--- WebApplication/src/app/article/create-article/create-article.component.ts
+++ WebApplication/src/app/article/create-article/create-article.component.ts
@@ -6,8 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-article.component.css']
 })
 export class CreateArticleComponent implements OnInit {
-  // name='Angular';
-  // value : number; result:number;
+ 
 
   constructor() {
    
@@ -15,27 +14,115 @@ export class CreateArticleComponent implements OnInit {
 
   ngOnInit() {
   }
-  public changeThis(){
+  public changeThis(TagName: string){
+    
     var unit = (<HTMLInputElement>document.getElementById("articleTags")).value; 
-    document.getElementById('newText').innerHTML = unit;
-    //document.body.appendChild(unit);
-   //document.documentElement.firstChild.appendChild(newText);
-    //document.body.appendChild();
+    
+    if(TagName != "")
+    unit = TagName;
+    
+    var oldtext1 = document.getElementById('newText1').innerHTML;
+    var oldtext2 = document.getElementById('newText2').innerHTML;
+    var oldtext3 = document.getElementById('newText3').innerHTML;
+
+    if(oldtext1 == "")
+    {document.getElementById('newText1').innerHTML = unit;}
+     else
+     {
+       if(oldtext2 == "")
+       {document.getElementById('newText1').innerHTML = unit;
+         document.getElementById('newText2').innerHTML = oldtext1;
+        }
+        else
+        {document.getElementById('newText1').innerHTML = unit;
+        document.getElementById('newText2').innerHTML = oldtext1;
+        document.getElementById('newText3').innerHTML = oldtext2;
+        }
+      }
+      console.log("working");
      
-    console.log("working");
        }
-      //  export class AppComponent 
-      //  { 
-      // name = 'Angular'; 
-      // value : number;value1 : number;result:number;
-      //  constructor(value : number,value1 : number,result:number)
-      //  {
-     
-      //  
-      //  }}
 
+       public changeThis2(){
+        var x = (<HTMLInputElement>document.getElementById("myTagBtn2")).value; 
+        document.getElementById('newText1').innerHTML = x;
+        // if(TagName1 != "")
+        // x = TagName1;
+        // var oldtext1 = document.getElementById('newText1').innerHTML;
+        // var oldtext2 = document.getElementById('newText2').innerHTML;
+        // var oldtext3 = document.getElementById('newText3').innerHTML;
+        // if(oldtext1 == "")
+        
+        // {
+        //   document.getElementById('newText1').innerHTML = x;
+        // }
+        //  else
+        //  {
+        //    if(oldtext2 == "")
+        //    {document.getElementById('newText1').innerHTML = x;
+        //      document.getElementById('newText2').innerHTML = oldtext1;
+        //     }
+        //     else
+        //     {document.getElementById('newText1').innerHTML = x;
+        //     document.getElementById('newText2').innerHTML = oldtext1;
+        //     document.getElementById('newText3').innerHTML = oldtext2;
+        //     }
+        //   }
+        }
+       changeThis3(){
+        var x = (<HTMLInputElement>document.getElementById("myTagBtn3")).value; 
+        document.getElementById('newText1').innerHTML = x;
+        console.log("tag is working");
 
+       }
+       changeThis4(){
+        var x = (<HTMLInputElement>document.getElementById("myTagBtn4")).value; 
+        document.getElementById('newText1').innerHTML = x;
+        console.log("tag is working");
 
+       }
+       changeThis5(){
+        var x = (<HTMLInputElement>document.getElementById("myTagBtn5")).value; 
+        document.getElementById('newText1').innerHTML = x;
+        console.log("tag is working");
+
+       }
+       changeThis6(){
+        var x = (<HTMLInputElement>document.getElementById("myTagBtn6")).value; 
+        document.getElementById('newText1').innerHTML = x;
+        console.log("tag is working");
+
+       }
+       changeThis7(){
+        var x = (<HTMLInputElement>document.getElementById("myTagBtn7")).value; 
+        document.getElementById('newText1').innerHTML = x;
+        console.log("tag is working");
+
+       }
+       changeThis8(){
+        var x = (<HTMLInputElement>document.getElementById("myTagBtn8")).value; 
+        document.getElementById('newText1').innerHTML = x;
+        console.log("tag is working");
+
+       }
+       changeThis9(){
+        var x = (<HTMLInputElement>document.getElementById("myTagBtn9")).value; 
+        document.getElementById('newText1').innerHTML = x;
+        console.log("tag is working");
+
+       }
+       changeThis10(){
+        var x = (<HTMLInputElement>document.getElementById("myTagBtn10")).value; 
+        document.getElementById('newText1').innerHTML = x;
+        console.log("tag is working");
+
+       }
+       changeThis11(){
+        var x = (<HTMLInputElement>document.getElementById("myTagBtn11")).value; 
+        document.getElementById('newText1').innerHTML = x;
+        console.log("tag is working");
+
+       }
 }
 
 
