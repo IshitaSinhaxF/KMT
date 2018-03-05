d82717b7e4872fdfd1f32b158d9f40ebabeedd66
Chandra Mahadevan <chandra.xfusion@gmail.com>
2018-03-05 11:37:36 -0800

    fixed pending approval ts for compilation

--- WebApplication/src/app/article/pending-for-approval/pending-for-approval.component.ts
+++ WebApplication/src/app/article/pending-for-approval/pending-for-approval.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ArticleService } from '../../article.service';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ArticleService} from '../../article.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-pending-for-approval',
@@ -9,30 +9,31 @@ import { Router } from '@angular/router';
 })
 export class PendingForApprovalComponent implements OnInit {
 
-  data: any = "";
-  result: any ;
-  userID: any = this.articleService.getFromSessionStorage("UserId"); 
+  data: any = '';
+  result: any;
+  userID: any = this.articleService.getFromSessionStorage('UserId');
   articleDetailsData: any = [];
-  id:number;
-  constructor(private articleService : ArticleService, private router : Router) { }
-  
+  id: number;
+
+  constructor(private articleService: ArticleService, private router: Router) {
+  }
+
   ngOnInit() {
 
-   console.log('User Id from sesssion: '+this.userID);
+    console.log('User Id from sesssion: ' + this.userID);
     this.userID = this.articleService.getInReviewArticleService(this.userID)
       .subscribe((res: Response) => {
         this.result = res;
         this.data = this.result.entries.entry;
-        console.log(this.data)
-      })
+        console.log(this.data);
+      });
   }
 
-   getArticleDetail(articleID) {
-      this.id = articleID;
-      this.articleService.saveInSessionStorage("articleID", this.id);
-      this.router.navigate(['./viewArticleInReview']);
+  getArticleDetail(articleID) {
+    this.id = articleID;
+    this.articleService.saveInSessionStorage('articleID', this.id);
+    this.router.navigate(['./viewArticleInReview']);
   }
 
-    
-  }
+
 }
