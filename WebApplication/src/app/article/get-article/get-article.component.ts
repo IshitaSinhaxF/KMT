387c48d6f62d73bb4382c796b47fe8ffc545c9db
Likhita <patralikhita@gmail.com>
2018-03-06 13:15:16 -0800

    Final changes made to get-article, draft, in-review, pending-for approval, published components

--- WebApplication/src/app/article/get-article/get-article.component.ts
+++ WebApplication/src/app/article/get-article/get-article.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
 import { ArticleService } from '../../article.service';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-get-article',
   templateUrl: './get-article.component.html',
@@ -12,36 +13,79 @@ import { Router } from '@angular/router';
 export class GetArticleComponent implements OnInit {
   data: any = "";
   result: any;
+  publishedData: any = "";
+  publishedResult: any = "";
+  pendingData: any = "";
+  pendingResult: any;
+  draftData: any = "";
+  draftResult: any = "";
+  inReviewData: any = "";
+  inReviewResult: any= "";
   articleDetailsData: any = [];
   setDiv: any;
   id: any;
+  userID: any = this.articleService.getFromSessionStorage('UserId');
+  inReviewUserId: any = this.articleService.getFromSessionStorage('UserId');
   roles: any = this.articleService.getFromSessionStorage("Roles");
   roleId: any = "";
   dataLength: any = "";
+  publishedDataLength: any = "";
+  pendingDataLength: any = "";
+  draftDataLength: any = "";
+  inReviewDataLength: any = "";
+
   constructor(private articleService: ArticleService, private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
-    if (this.roles != "") {
-      for (var i = 0; i < this.roles.length; i++) {
-        this.roleId = this.roles[i].lkpRoleID;
-        console.log('role id: ' + this.roleId);
-      }
-    }
+
 
     this.articleService.getPublishArticleService()
       .subscribe((res: Response) => {
-        this.result = res;
-        this.data = this.result.entries.entry;
+        this.publishedResult = res;
+        this.publishedData= this.publishedResult.entries.entry;
         this.setDiv = 'Published';
         this.id = 1;
-        console.log(this.data)
+        // console.log(this.data);
+        this.publishedDataLength = this.publishedData.length;
       })
 
-      this.dataLength = this.data.length;
-      console.log('data length: '+this.dataLength);
+      
+    this.articleService.getDraftArticleService(this.userID)
+    .subscribe((res: Response) => {
+      this.draftResult = res;
+      this.draftData = this.draftResult.entries.entry;
+      // console.log(this.data);
+      this.draftDataLength = this.draftData.length;
+      // console.log('length: ' + this.draftDataLength)
+    })
+
+
+    if (this.roles != "") {
+      for (var i = 0; i < this.roles.length; i++) {
+        this.roleId = this.roles[i].lkpRoleID;
+        // console.log('role id: ' + this.roleId);
+        if (this.roleId == 2) {
+          this.userID = this.articleService.getPendingArticleService(this.userID, this.roleId)
+            .subscribe((res: Response) => {
+              this.pendingResult = res;
+              this.pendingData = this.pendingResult.entries.entry;
+              this.pendingDataLength = this.pendingData.length
+            });
+        } else if (this.roleId == 1) {
+          this.inReviewUserId = this.articleService.getInReviewArticleService(this.inReviewUserId, this.roleId)
+            .subscribe((res: Response) => {
+              this.inReviewResult = res;
+              this.inReviewData = this.inReviewResult.entries.entry;
+              this.inReviewDataLength = this.inReviewData.length
+            });
+        }
+      }
+    }
+
+
   }
 
-  
+
 
   displayPublishedDiv() {
     console.log('published')
