60180cdcc0596565b5313224ec1f7eb584b4eb1b
Ananya2017 <anandi@xfusiontech.com>
2018-03-08 14:17:52 -0800

    Feedback CSS changes

--- WebApplication/src/app/app.module.ts
+++ WebApplication/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxEditorModule } from 'ngx-editor';
 
 import { AppRoutingModule } from './/app-routing.module';
 import { AppComponent } from './app.component';
@@ -56,7 +57,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
    //AngularFontAwesomeModule
   ],
   imports: [
-
+    NgxEditorModule,
     BrowserModule,
     AppRoutingModule,
     RouterModule,
