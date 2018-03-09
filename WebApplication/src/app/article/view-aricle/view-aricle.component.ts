c9859e35d5ac810556d12a25ad9465a9f44a17a3
Chandra Mahadevan <chandra.xfusion@gmail.com>
2018-03-09 02:14:45 -0800

    Missing } error. fixed

--- WebApplication/src/app/article/view-aricle/view-aricle.component.ts
+++ WebApplication/src/app/article/view-aricle/view-aricle.component.ts
@@ -9,36 +9,38 @@ import { Router } from '@angular/router';
   styleUrls: ['./view-aricle.component.css']
 })
 export class ViewAricleComponent implements OnInit {
- articleID: any = this.articleService.getFromSessionStorage("articleID");
- userID: number = +this.articleService.getFromSessionStorage("UserId"); 
- userName: string = this.articleService.getFromSessionStorage("UserName");
- result: any;
- articleDetailsData: any = [];
+  articleID: any = this.articleService.getFromSessionStorage("articleID");
+  userID: number = +this.articleService.getFromSessionStorage("UserId");
+  userName: string = this.articleService.getFromSessionStorage("UserName");
+  result: any;
+  articleDetailsData: any = [];
 //  articleDetailsFeedbacks: any = [];
- articleFeedbackText : string;
+  articleFeedbackText: string;
 //  txtFeedbck : any = "";
- artID: number;
- insertStatus: any;
- 
- 
-constructor(private articleService : ArticleService, private router: Router) { }
-ngOnInit() {
+  artID: number;
+  insertStatus: any;
 
-      this.articleService.getArticleDetailsById(this.articleID).subscribe((res: Response) => {
-      this.result = res;
-      this.articleDetailsData = this.result.entries.entry[0];  
-      console.log(this.articleDetailsData);
-    })    
+
+  constructor(private articleService: ArticleService, private router: Router) {
   }
 
-    onbackToHome() {
+  ngOnInit() {
+
+    this.articleService.getArticleDetailsById(this.articleID).subscribe((res: Response) => {
+      this.result = res;
+      this.articleDetailsData = this.result.entries.entry[0];
+      console.log(this.articleDetailsData);
+    })
+  }
+
+  onbackToHome() {
     this.router.navigate(['./home']);
   }
 
- logOut(){
-    this.articleService.removeFromSessionStorage("Roles") ;
-    this.articleService.removeFromSessionStorage("UserId") ;
-    this.articleService.removeFromSessionStorage("UserName") ;
+  logOut() {
+    this.articleService.removeFromSessionStorage("Roles");
+    this.articleService.removeFromSessionStorage("UserId");
+    this.articleService.removeFromSessionStorage("UserName");
     this.router.navigate(['']);
   }
-
+}
